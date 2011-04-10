631d8ea5ea7732034638b939b111804a9dfaf046
stefan haustein <stefan.haustein@gmail.com>
2011-04-10 13:49:28 +0200

    formatting

--- demo/com/google/code/infusion/demo/simple/SimpleDemo.java
+++ demo/com/google/code/infusion/demo/simple/SimpleDemo.java
@@ -12,100 +12,100 @@ import com.google.code.infusion.util.AsyncCallback;
 import com.google.code.infusion.util.ClientLogin;
 
 public class SimpleDemo {
-	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	static FusionTableService service;
-	
-	public static void main(String[] args) throws IOException {
-		
-		System.out.print("Username (email): ");
-		String user = reader.readLine();
-		if (user == null || user.length() == 0) {
-			return;
-		}
+  static BufferedReader reader = new BufferedReader(new InputStreamReader(
+      System.in));
+  static FusionTableService service;
 
-		System.out.print("Password: ");
-		String password = reader.readLine();
-		if (password == null || password.length() == 0) {
-			return;
-		}
-		System.out.println("\f");
+  public static void main(String[] args) throws IOException {
 
-		ClientLogin.requestAuthToken(ClientLogin.ACCOUNT_TYPE_GOOGLE, 
-				user, password, "fusiontables", "GoogleCodeProjectInfusion-Infusion-0.1", new AsyncCallback<String>() {
-			public void onSuccess(String result) {
-				runSession(result);
-			}
-		
-			public void onFailure(Throwable caught) {
-				caught.printStackTrace();
-				return;
-			}
-		});
-	}
-	
-	
-	private static void runSession(String authToken) {
-		System.out.println("Authenticated Sucessfully; Auth token: " + authToken);
-		service = new FusionTableService(authToken);
-		showPrompt();
-		while (true) {
-			try {
-				String cmd = reader.readLine();
-				if (cmd == null || cmd.length() == 0) {
-					break;
-				} else if ("?".equals(cmd) || "help".equals(cmd)) {
-					System.out.println("show tables:         List available tables");
-					System.out.println("describe <table id>: Show table structure");
-				} else if ("show tables".equals(cmd)) {
-					showTables();
-				} else if (cmd.startsWith("describe ")) {
-					describe(cmd.substring(9).trim());
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	private static void describe(String tableId) {
-		service.describe(tableId, new AsyncCallback<List<ColumnInfo>>() {
-			public void onFailure(Throwable caught) {
-				showError(caught);
-			}
+    System.out.print("Username (email): ");
+    String user = reader.readLine();
+    if (user == null || user.length() == 0) {
+      return;
+    }
 
-			public void onSuccess(List<ColumnInfo> result) {
-				System.out.println("Columns:");
-				for(ColumnInfo ci: result) {
-					System.out.println(ci);
-				}
-				showPrompt();		
-			}
-		});
-	}
-	
-	private static void showTables() {	
-		service.showTables(new AsyncCallback<List<TableInfo>>() {
-			public void onFailure(Throwable caught) {
-				showError(caught);
-			}
+    System.out.print("Password: ");
+    String password = reader.readLine();
+    if (password == null || password.length() == 0) {
+      return;
+    }
+    System.out.println(((char) 27) + "[2J\f");
 
-			public void onSuccess(List<TableInfo> result) {
-				System.out.println("Tables available:");
-				for(TableInfo td: result) {
-					System.out.println(td);
-				}
-				showPrompt();			
-			}
-		});
-	}
-	
-	private static void showPrompt() {
-		System.out.print("Command ('help' for help)> ");
-	}
+    ClientLogin.requestAuthToken(ClientLogin.ACCOUNT_TYPE_GOOGLE, user,
+        password, "fusiontables", "GoogleCodeProjectInfusion-Infusion-0.1",
+        new AsyncCallback<String>() {
+          public void onSuccess(String result) {
+            runSession(result);
+          }
 
+          public void onFailure(Throwable caught) {
+            caught.printStackTrace();
+            return;
+          }
+        });
+  }
 
-	private static void showError(Throwable caught) {
-		caught.printStackTrace();
-		showPrompt();
-	}
+  private static void runSession(String authToken) {
+    System.out.println("Authenticated Sucessfully; Auth token: " + authToken);
+    service = new FusionTableService(authToken);
+    showPrompt();
+    while (true) {
+      try {
+        String cmd = reader.readLine();
+        if (cmd == null || cmd.length() == 0) {
+          break;
+        } else if ("?".equals(cmd) || "help".equals(cmd)) {
+          System.out.println("show tables:         List available tables");
+          System.out.println("describe <table id>: Show table structure");
+        } else if ("show tables".equals(cmd)) {
+          showTables();
+        } else if (cmd.startsWith("describe ")) {
+          describe(cmd.substring(9).trim());
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private static void describe(String tableId) {
+    service.describe(tableId, new AsyncCallback<List<ColumnInfo>>() {
+      public void onFailure(Throwable caught) {
+        showError(caught);
+      }
+
+      public void onSuccess(List<ColumnInfo> result) {
+        System.out.println("Columns:");
+        for (ColumnInfo ci : result) {
+          System.out.println(ci);
+        }
+        showPrompt();
+      }
+    });
+  }
+
+  private static void showTables() {
+    service.showTables(new AsyncCallback<List<TableInfo>>() {
+      public void onFailure(Throwable caught) {
+        showError(caught);
+      }
+
+      public void onSuccess(List<TableInfo> result) {
+        System.out.println("Tables available:");
+        for (TableInfo td : result) {
+          System.out.println(td);
+        }
+        showPrompt();
+      }
+    });
+  }
+
+  private static void showPrompt() {
+    System.out.print("Command ('help' for help)> ");
+  }
+
+  private static void showError(Throwable caught) {
+    caught.printStackTrace();
+    showPrompt();
+  }
 }
