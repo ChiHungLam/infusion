792634e886c84822653b0f0be2ce712acffbfc52
stefan haustein <stefan.haustein@gmail.com>
2011-04-10 13:43:08 +0200

    some progress towards bibtex import via. UI

---
+++ demo/com/google/code/infusion/demo/gwt/client/Blob.java
@@ -0,0 +1,7 @@
+package com.google.code.infusion.demo.gwt.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class Blob extends JavaScriptObject {
+
+}
--- demo/com/google/code/infusion/demo/gwt/client/InfusionGwtDemo.java
+++ demo/com/google/code/infusion/demo/gwt/client/InfusionGwtDemo.java
@@ -34,88 +34,87 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  */
 public class InfusionGwtDemo implements EntryPoint {
 
-	private FusionTableService service;
+  private FusionTableService service;
 
-	DockLayoutPanel mainPanel = new DockLayoutPanel(Unit.PX);
-	SimplePanel loginPanel = new SimplePanel();
-	VerticalPanel tableListPanel = new VerticalPanel();
-	
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {
-		Document.get().getElementById("loading").removeFromParent();
-		
-		Grid grid = new Grid(3, 2);
-				
-		grid.setWidget(0, 0, new Label("Email Address:"));
-		grid.setWidget(1, 0, new Label("Password:"));
-		
-		final TextBox email = new TextBox();
-		final PasswordTextBox password = new PasswordTextBox();
+  DockLayoutPanel mainPanel = new DockLayoutPanel(Unit.PX);
+  SimplePanel loginPanel = new SimplePanel();
+  VerticalPanel tableListPanel = new VerticalPanel();
 
-		grid.setWidget(0, 1, email);
-		grid.setWidget(1, 1, password);
 
-		final Button loginButton = new Button("Login");
-		loginButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				ClientLogin.requestAuthToken(
-						ClientLogin.ACCOUNT_TYPE_GOOGLE, 
-						email.getValue(), 
-						password.getValue(), 
-						"fusiontables", 
-						"GoogleCodeProjectInfusion-InfusionDemo-0.1",
-						new AsyncCallback<String>() {
-					public void onSuccess(String result) {
-						authenticationObtained(result);
-					}
-					
-					public void onFailure(Throwable caught) {
-						Window.alert(caught.getMessage());
-					}
-				});
-			}});
-	    grid.setWidget(2, 1, loginButton);
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+    Document.get().getElementById("loading").removeFromParent();
 
-	    loginPanel.add(grid);
-	    
-		RootLayoutPanel.get().add(mainPanel);
-		mainPanel.addWest(tableListPanel, 150);
-		mainPanel.add(loginPanel);
-	}
-	
-	static native String loadFile(NativeEvent event) /*-{
-		
-	}-*/;
-	
-	void authenticationObtained(String authtoken) {
-		this.service = new FusionTableService(authtoken);
-		service.showTables(new AsyncCallback<List<TableInfo>>() {
+    Grid grid = new Grid(3, 2);
 
-			public void onFailure(Throwable caught) {
-				Window.alert(caught.getLocalizedMessage());
-			}
+    grid.setWidget(0, 0, new Label("Email Address:"));
+    grid.setWidget(1, 0, new Label("Password:"));
 
-			public void onSuccess(List<TableInfo> result) {
-				for (TableInfo table : result) {
-					tableListPanel.add(new Label(table.getName()));
-				}
-				
-				FileUpload upload = new FileUpload();
-				tableListPanel.add(upload);
-				upload.addChangeHandler(new ChangeHandler() {
+    final TextBox email = new TextBox();
+    final PasswordTextBox password = new PasswordTextBox();
 
-					public void onChange(ChangeEvent event) {
-						String content = loadFile(event.getNativeEvent());
-						Window.alert(content);
-					}
-					
-				});
-			}
-		});
-		
-		
-	}
-	
+    grid.setWidget(0, 1, email);
+    grid.setWidget(1, 1, password);
+
+    final Button loginButton = new Button("Login");
+    loginButton.addClickHandler(new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        ClientLogin.requestAuthToken(ClientLogin.ACCOUNT_TYPE_GOOGLE,
+            email.getValue(), password.getValue(), "fusiontables",
+            "GoogleCodeProjectInfusion-InfusionDemo-0.1",
+            new AsyncCallback<String>() {
+              public void onSuccess(String result) {
+                authenticationObtained(result);
+              }
+
+              public void onFailure(Throwable caught) {
+                Window.alert(caught.getMessage());
+              }
+            });
+      }
+    });
+    grid.setWidget(2, 1, loginButton);
+
+    loginPanel.add(grid);
+
+    RootLayoutPanel.get().add(mainPanel);
+    mainPanel.addWest(tableListPanel, 150);
+    mainPanel.add(loginPanel);
+  }
+
+
+  static native String loadFile(NativeEvent event) /*-{
+    var input = event.target;
+    var file = input.files[0];
+    return file.name;
+  }-*/;
+
+
+  void authenticationObtained(String authtoken) {
+    this.service = new FusionTableService(authtoken);
+    service.showTables(new AsyncCallback<List<TableInfo>>() {
+
+      public void onFailure(Throwable caught) {
+        Window.alert(caught.getLocalizedMessage());
+      }
+
+      public void onSuccess(List<TableInfo> result) {
+        for (TableInfo table : result) {
+          tableListPanel.add(new Label(table.getName()));
+        }
+
+        FileUpload upload = new FileUpload();
+        tableListPanel.add(upload);
+        upload.addChangeHandler(new ChangeHandler() {
+
+          public void onChange(ChangeEvent event) {
+            String content = loadFile(event.getNativeEvent());
+            Window.alert(content);
+          }
+        });
+      }
+    });
+  }
 }
