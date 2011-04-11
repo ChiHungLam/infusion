66146a1f0bd32aff635a134549a35b145d6d0a97
stefan haustein <stefan.haustein@gmail.com>
2011-04-11 17:58:12 +0200

    Formatting, visibility

--- src/com/google/code/infusion/datastore/ColumnInfo.java
+++ src/com/google/code/infusion/datastore/ColumnInfo.java
@@ -1,23 +1,23 @@
 package com.google.code.infusion.datastore;
 
 public class ColumnInfo {
-	private String name;
-	private ColumnType<?> type;
-	
-	public ColumnInfo(String name, ColumnType<?> type) {
-		this.name = name;
-		this.type = type;
-	}
-	
-	String getName() {
-		return name;
-	}
-	
-	ColumnType<?> getType() {
-		return type;
-	}
-	
-	public String toString() {
-	  return name + ": " + type;
-	}
+  private final String name;
+  private final ColumnType<?> type;
+
+  public ColumnInfo(String name, ColumnType<?> type) {
+    this.name = name;
+    this.type = type;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public ColumnType<?> getType() {
+    return type;
+  }
+
+  public String toString() {
+    return name + ": " + type;
+  }
 }
