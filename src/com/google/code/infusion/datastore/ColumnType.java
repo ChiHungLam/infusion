00c66d16384fe0a6af2fc685d9f823cdd8a08b59
stefan haustein <stefan.haustein@gmail.com>
2011-04-11 17:59:21 +0200

    Formatting

--- src/com/google/code/infusion/datastore/ColumnType.java
+++ src/com/google/code/infusion/datastore/ColumnType.java
@@ -2,42 +2,42 @@ package com.google.code.infusion.datastore;
 
 public interface ColumnType<T> {
 
-	static final ColumnType<String> STRING = new AbstractColumnType<String>(
-			"STRING") {
-		public String parse(String s) {
-			return s;
-		}
-	};
+  static final ColumnType<String> STRING = new AbstractColumnType<String>(
+      "STRING") {
+    public String parse(String s) {
+      return s;
+    }
+  };
 
-	String getName();
+  String getName();
 
-	T parse(String s);
+  T parse(String s);
 
-	String toString(T value);
+  String toString(T value);
 
-	String getBaseType();
-	
-	static abstract class AbstractColumnType<T> implements ColumnType<T> {
-		private final String name;
+  String getBaseType();
 
-		private AbstractColumnType(String name) {
-			this.name = name;
-		}
+  static abstract class AbstractColumnType<T> implements ColumnType<T> {
+    private final String name;
 
-		public String getName() {
-			return name;
-		}
+    private AbstractColumnType(String name) {
+      this.name = name;
+    }
 
-		public String toString() {
-			return name;
-		}
-		
-		public String getBaseType() {
-		  return "STRING";
-		}
+    public String getName() {
+      return name;
+    }
 
-		public String toString(T value) {
-			return value.toString();
-		}
-	}
+    public String toString() {
+      return name;
+    }
+
+    public String getBaseType() {
+      return "STRING";
+    }
+
+    public String toString(T value) {
+      return value.toString();
+    }
+  }
 }
