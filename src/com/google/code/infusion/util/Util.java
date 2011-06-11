38f55bed92a4d1bdacf97543ad87d5910d784ffc
stefan haustein <stefan.haustein@gmail.com>
2011-06-11 22:46:48 +0200

    gwt compatibiltiy fix

--- src/com/google/code/infusion/util/Util.java
+++ src/com/google/code/infusion/util/Util.java
@@ -1,6 +1,7 @@
 package com.google.code.infusion.util;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Map;
@@ -114,57 +115,9 @@ public class Util {
         if (cut == -1) {
           result.put(part, "");
         } else {
-          result.put(part.substring(0, cut), Util.urlDecode(part.substring(cut + 1)));
+          result.put(part.substring(0, cut), urlDecode(part.substring(cut + 1)));
         }
       }
-    }
-  }
-
-  public static String urlEncode(String s) {
-    byte[] b;
-    try {
-      b = s.getBytes("utf-8");
-  
-    int len = b.length;
-    StringBuilder sb = new StringBuilder(len * 3 / 2);
-    for (int i = 0; i < len; i++) {
-      char c = (char) (b[i] & 255);
-      if ((c >= '0' && c <= '9') || 
-          (c >= 'A' && c <= 'Z') ||
-          (c >= 'a' && c <= 'z') || 
-          c == '_' || c == '.' || c == '~' || c == '-') {
-        sb.append(c);
-      } else {
-        sb.append('%');
-        sb.append(Util.HEX_DIGITS.charAt(c / 16));
-        sb.append(Util.HEX_DIGITS.charAt(c % 16));
-      }
-    }
-    return sb.toString();
-    } catch (UnsupportedEncodingException e) {
-     throw new RuntimeException(e);
-    }
-  }
-
-  public static String urlDecode(String s) {
-    int len = s.length();
-    byte[] b = new byte[len];
-    int pos = 0;
-    for (int i = 0; i < len; i++) {
-      char c = s.charAt(i);
-      if (c == '%') {
-        b[pos++] = (byte) Integer.parseInt(s.substring(i+1, i+3), 16);
-        i+=2;
-      } else if (c == '+') {
-        b[pos++] = 32;
-      } else {
-        b[pos++] = (byte) c;
-      }
-    }
-    try {
-      return new String(b, 0, pos, "UTF-8");
-    } catch (UnsupportedEncodingException e) {
-     throw new RuntimeException(e);
     }
   }
 
@@ -212,11 +165,19 @@ public class Util {
     return sb.toString();
   }
 
- /* public static String urlEncode(String url) {
+  public static String urlDecode(String url) {
+    try {
+      return URLDecoder.decode(url, "utf-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  public static String urlEncode(String url) {
     try {
       return URLEncoder.encode(url, "utf-8");
     } catch (UnsupportedEncodingException e) {
       throw new RuntimeException(e);
     }
-  } */
+  }
 }
