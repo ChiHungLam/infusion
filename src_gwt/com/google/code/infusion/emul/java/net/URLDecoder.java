3817040a01b84646a4d025dbf8a24db1b78d0fb2
stefan haustein <stefan.haustein@gmail.com>
2011-06-11 22:58:06 +0200

    mini demo added. cleanup

--- src_gwt/com/google/code/infusion/emul/java/net/URLDecoder.java
+++ src_gwt/com/google/code/infusion/emul/java/net/URLDecoder.java
@@ -3,11 +3,10 @@ package java.net;
 import java.io.UnsupportedEncodingException;
 
 public class URLDecoder {
-    public static String decode(String url, String charset) throws java.io.UnsupportedEncodingException {
-        if (!charset.equalsIgnoreCase("utf-8")) {
-            throw new UnsupportedEncodingException();
-        }
-        return com.google.gwt.http.client.URL.decodeQueryString(url);
+  public static String decode(String url, String charset) throws java.io.UnsupportedEncodingException {
+    if (!charset.equalsIgnoreCase("utf-8")) {
+      throw new UnsupportedEncodingException();
     }
-  
+    return com.google.gwt.http.client.URL.decodeQueryString(url);
+  }
 }
