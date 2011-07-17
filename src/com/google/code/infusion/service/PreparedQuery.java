b3e245784fdcd34dfcc7547f62c36d23d7e3c2db
stefan haustein <stefan.haustein@gmail.com>
2011-07-17 19:53:11 +0200

    Count added, redundant emul class removed

--- src/com/google/code/infusion/service/PreparedQuery.java
+++ src/com/google/code/infusion/service/PreparedQuery.java
@@ -1,5 +1,8 @@
 package com.google.code.infusion.service;
 
+import java.util.List;
+
+import com.google.code.infusion.util.ChainedCallback;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class PreparedQuery {
@@ -12,7 +15,7 @@ public class PreparedQuery {
     this.query = query;
   }
 
-  public void asTable(FetchOptions fetchOptions, AsyncCallback<Table> callback) {
+  private String withFetchOptions(FetchOptions fetchOptions) {
     StringBuilder sb = new StringBuilder(query);
     if (fetchOptions.getOffset() != 0) {
       sb.append(" OFFSET ");
@@ -22,11 +25,26 @@ public class PreparedQuery {
       sb.append(" LIMIT ");
       sb.append(fetchOptions.getLimit());
     }
-    service.query(sb.toString(), callback);
+    return sb.toString();
+  }
+  
+  public void asTable(FetchOptions fetchOptions, AsyncCallback<Table> callback) {
+    service.query(withFetchOptions(fetchOptions), callback);
   }
   
   public void asTable(AsyncCallback<Table> callback) {
     service.query(query, callback);
   }
-  
+
+  public void countEntities(FetchOptions fetchOptions, final AsyncCallback<Integer> callback) {
+    String q = withFetchOptions(fetchOptions);
+    int cut = q.indexOf("FROM");
+    q = "SELECT ROWID " + q.substring(cut);
+    service.query(q,  new ChainedCallback<Table>(callback) {
+      @Override
+      public void onSuccess(Table result) {
+        callback.onSuccess(result.getRowCount());
+      }
+    });
+  }
 }
